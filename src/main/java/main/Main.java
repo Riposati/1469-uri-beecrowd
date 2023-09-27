b5ed0b0552f75b1db4183eddd12cdee016eb7f27
Gustavo <guriposa@gmail.com>
2023-09-27 19:41:53 -0300

    improvements

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -94,17 +94,13 @@ public class Main {
         Graph graph = new Graph();
 
         while ((str = in.readLine()) != null) {
-
             N = Integer.parseInt(str.split(" ")[0]);
             M = Integer.parseInt(str.split(" ")[1]);
             I = Integer.parseInt(str.split(" ")[2]);
 
             initializeGraphData(N, M, graph);
-
             readAges(in, N, graph);
-
             readAjacentList(in, M, graph);
-
             operations(in, N, I, graph);
         }
     }
@@ -124,47 +120,47 @@ public class Main {
     }
 
     private static void readAges(BufferedReader in, int N, Graph graph) throws IOException {
-        String str2 = in.readLine();
+        String aux = in.readLine();
         for (int i = 0; i < N; i++) {
-            graph.getId().set(i, Integer.parseInt(str2.split(" ")[i]));
+            graph.getId().set(i, Integer.parseInt(aux.split(" ")[i]));
             graph.getPos().set(i, i);
         }
     }
 
     private static void operations(BufferedReader in, int N, int I, Graph graph) throws IOException {
         for (int i = 0; i < I; i++) {
-            String aux2 = in.readLine();
-            String comando = aux2.split(" ")[0];
+            String aux = in.readLine();
+            String command = aux.split(" ")[0];
 
-            if (comando.equalsIgnoreCase("P")) {
-                analyseLowestAge(N, graph, aux2);
+            if (command.equalsIgnoreCase("P")) {
+                analyseLowestAge(N, graph, aux);
             } else {
-                swapPlaces(graph, aux2);
+                swapPlaces(graph, aux);
             }
         }
     }
 
     private static void swapPlaces(Graph graph, String aux2) {
-        int vertice1 = Integer.parseInt(aux2.split(" ")[1]);
-        int vertice2 = Integer.parseInt(aux2.split(" ")[2]);
+        int vertexOne = Integer.parseInt(aux2.split(" ")[1]);
+        int verticeTwo = Integer.parseInt(aux2.split(" ")[2]);
 
-        Collections.swap(graph.getId(), graph.getPos().get(vertice1 - 1), graph.getPos().get(vertice2 - 1));
-        Collections.swap(graph.getPos(), vertice1 - 1, vertice2 - 1);
+        Collections.swap(graph.getId(), graph.getPos().get(vertexOne - 1), graph.getPos().get(verticeTwo - 1));
+        Collections.swap(graph.getPos(), vertexOne - 1, verticeTwo - 1);
     }
 
     private static void analyseLowestAge(int N, Graph graph, String aux2) {
-        int vertice = Integer.parseInt(aux2.split(" ")[1]);
+        int vertex = Integer.parseInt(aux2.split(" ")[1]);
 
-        if (graph.getAdj().get(graph.getPos().get(vertice - 1)).isEmpty())
+        if (graph.getAdj().get(graph.getPos().get(vertex - 1)).isEmpty())
             System.out.print("*\n");
         else {
 
             graph.initializeVis(N);
 
-            int menor = 999;
-            menor = graph.depthFirstSearch(graph.getPos().get(vertice - 1), menor);
+            int lowest = 999;
+            lowest = graph.depthFirstSearch(graph.getPos().get(vertex - 1), lowest);
 
-            System.out.print(menor + "\n");
+            System.out.print(lowest + "\n");
         }
     }
 }
